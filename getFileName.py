bb790a6ee71df5ff89fb218157f18d55dac15c84
michael <hirsch617@gmail.com>
2015-05-29 20:09:56 -0400

    cleanup

--- getFileName.py
+++ getFileName.py
@@ -1,37 +1,38 @@
+from madrigalWeb import madrigalWeb as MW
+from os.path import join
+# globals
+madfiltfn = "filestodownload.txt"
+limit = 100
 
 def getFiles(filename,limit):
 
-    rawfile = open(filename)
-    i=0
-    files = []
-    print 'finding files'
+    with open(filename,'r') as fraw:
+        i=0
+        files = []
+        for line in fraw:
+            if i>limit: break
 
-    while i<limit:
-        line = rawfile.readline()
-
-        if '\n' not in line:
-            return files
-
-        if len(line)>50:
-            nextline = rawfile.readline()
-            if 'No records were selected with the filters above' not in nextline:
+            if line.startswith('/opt/madrigal'):
                 files.append(line.strip())
-                print i
                 i+=1
 
-    rawfile.close()
     return files
+#%% main program
+from argparse import ArgumentParser
+p = ArgumentParser(description='scan Madrigal with criteria')
+p.add_argument('username',help='your name',nargs='?',default='guest')
+p.add_argument('email',help='your email',nargs='?',default='guest')
+p.add_argument('institution',help='your institution',nargs='?',default='Acme, Inc.')
+p.add_argument('--download',help='type of file to download',default='hdf5')
+p.add_argument('--madurl',help='madrigal server url',default='http://isr.sri.com/madrigal/')
+p = p.parse_args()
 
+print('connecting to {}'.format(p.madurl))
+site = MW.MadrigalData(p.madurl)
+print('saving Madrigal filter output to {}'.format(madfiltfn))
+files = getFiles(madfiltfn,limit)
 
-from madrigalWeb import madrigalWeb as MW
-print('connecting to site')
-site = MW.MadrigalData('http://isr.sri.com/madrigal/')
-filename = "filestodownload.txt"
-limit = 100
-print('getFiles')
-files = getFiles(filename,limit)
-
-print 'downloading files'
 for f in files:
-    print(files.index(f))
-    site.downloadFile(f,"files/"+str(files.index(f))+".h5",username,email,institution,"hdf5")
+    print('processing {}'.format(files.index(f)))
+    site.downloadFile(f, join("files",str(files.index(f))+".h5"),
+                      p.username,p.email,p.institution,p.download)
