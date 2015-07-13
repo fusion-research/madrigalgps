6c64bf6ffe3e91b15d545e0a37fb42c67a93d4fc
Michael <hirsch617@gmail.com>
2015-07-13 14:05:06 -0400

    functionalize

--- getFileName.py
+++ getFileName.py
@@ -4,7 +4,6 @@ from os.path import join,basename
 from os import mkdir
 from time import time
 # globals
-madfiltfn = "enddown.txt"
 limit = 100
 
 def getFiles(filename,limit):
@@ -19,28 +18,30 @@ def getFiles(filename,limit):
 
     return files
 #%% main program
-from argparse import ArgumentParser
-p = ArgumentParser(description='scan Madrigal with criteria')
-p.add_argument('username',help='your name',nargs='?',default='guest')
-p.add_argument('email',help='your email',nargs='?',default='guest')
-p.add_argument('institution',help='your institution',nargs='?',default='Acme, Inc.')
-p.add_argument('--download',help='type of file to download',default='hdf5')
-p.add_argument('--madurl',help='madrigal server url',default='http://isr.sri.com/madrigal/')
-p.add_argument('--outdir',help='path to save madrigal data to',default='files')
-p = p.parse_args()
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser(description='scan Madrigal with criteria')
+    p.add_argument('filtername',help='madrigal filter filename (local file)',nargs='?',default='enddown.txt')
+    p.add_argument('-u','--username',help='your name',default='guest')
+    p.add_argument('-e','--email',help='your email',default='guest')
+    p.add_argument('-i','--institution',help='your institution',default='nobody')
+    p.add_argument('--download',help='type of file to download',default='hdf5')
+    p.add_argument('--madurl',help='madrigal server url',default='http://isr.sri.com/madrigal/')
+    p.add_argument('--outdir',help='path to save madrigal data to',default='files')
+    p = p.parse_args()
 
-tic = time()
-files = getFiles(madfiltfn,limit)
-print('filtered file list in {:.4f} sec.'.format(time()-tic))
-try:
-    mkdir(p.outdir)
-except OSError: #directory already exists
-    pass
-#%%
-print('connecting to {}'.format(p.madurl))
-site = MW.MadrigalData(p.madurl)
+    tic = time()
+    files = getFiles(p.filtername,limit)
+    print('filtered file list in {:.4f} sec.'.format(time()-tic))
+    try:
+        mkdir(p.outdir)
+    except OSError: #directory already exists
+        pass
+    #%%
+    print('connecting to {}'.format(p.madurl))
+    site = MW.MadrigalData(p.madurl)
 
-for f in files:
-    print('processing {}'.format(f))
-    site.downloadFile(f, join(p.outdir,basename(f)+'.h5'),
-                      p.username,p.email,p.institution,p.download)
+    for f in files:
+        print('processing {}'.format(f))
+        site.downloadFile(f, join(p.outdir,basename(f)+'.h5'),
+                          p.username,p.email,p.institution,p.download)
